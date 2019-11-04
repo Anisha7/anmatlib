564f30e49be30d2892cf143d136a561b8e26e398
Anisha Jain <anishajain@outlook.com>
2019-11-04 00:49:10 -0800

    number methods added

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 
 // Adds a goldenRatio property to Number
 Number.prototype.goldenRatio = 1.61803398875
+
+Number.prototype.round = function () {
+    return Math.round(this)
+}
+
+Number.prototype.floor = function () {
+    return Math.floor(this)
+}
+
+Number.prototype.ceil = function () {
+    return Math.ceil(this)
+}
--- test.js
+++ test.js
@@ -1,5 +1,21 @@
-const FL = require('./index.js');
+require('./index.js');
 
-test('sample', () => {
-    expect(true).toBe(true);
+test('goldenRatio', () => {
+    const x = 5
+    expect(x.goldenRatio).toBe(1.61803398875);
+});
+
+test('round', () => {
+    const x = 5.9
+    expect(x.round()).toBe(6);
+});
+
+test('floor', () => {
+    const x = 5.9
+    expect(x.floor()).toBe(5);
+});
+
+test('ceil', () => {
+    const x = 1.03
+    expect(x.ceil()).toBe(2);
 });
